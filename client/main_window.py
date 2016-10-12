813e7a9943112a14f26f0f47bfbf5986ceabe1f3
ZICHEN ZHU <Frederick-Zhu@users.noreply.github.com>
2016-10-13 00:28:50 +0800

    每日commit 新建了工程 完成了主窗口的布局 还未绑定信号 仅是外观 完成了每个屏幕编号的显示 实现了无边框 透明背景 鼠标事件透传

---
+++ client/main_window.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+
+import sys
+
+from PyQt4 import QtGui
+
+import screen_test_window
+
+
+class MainWindow(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+
+        layout = QtGui.QVBoxLayout(self)
+
+        h_boxlayout = QtGui.QHBoxLayout()
+        h_boxlayout.addWidget(QtGui.QLabel('Server:'))
+        self.serverLineEdit = QtGui.QLineEdit()
+        self.serverLineEdit.setText('https://www.google.com')
+        h_boxlayout.addWidget(self.serverLineEdit)
+        layout.addLayout(h_boxlayout)
+
+        h_boxlayout = QtGui.QHBoxLayout()
+        h_boxlayout.addWidget(QtGui.QLabel('Password:'))
+        self.passwordLineEdit = QtGui.QLineEdit()
+        self.passwordLineEdit.setEchoMode(QtGui.QLineEdit.Password)
+        self.passwordLineEdit.setText('123')
+        h_boxlayout.addWidget(self.passwordLineEdit)
+        layout.addLayout(h_boxlayout)
+
+        h_boxlayout = QtGui.QHBoxLayout()
+        self.hideButton = QtGui.QPushButton('&Hide')
+        h_boxlayout.addWidget(self.hideButton)
+        self.startButton = QtGui.QPushButton('&Start')
+        h_boxlayout.addWidget(self.startButton)
+        layout.addLayout(h_boxlayout)
+
+        self.setLayout(layout)
+        self.resize(600, 200)
+        self.setWindowTitle('config')
+
+        self.initScreenTestWindow()
+
+        self.show()
+
+    def initScreenTestWindow(self):
+        screen_count = QtGui.QDesktopWidget().screenCount()
+        for screen_id in range(screen_count):
+            print screen_id
+            screen_test_window.ScreenTestWindow(self, screen_id)
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+
+    main_window = MainWindow()
+
+    sys.exit(app.exec_())
